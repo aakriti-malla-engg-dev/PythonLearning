636ba65354ff211983258e375011f1676a675143
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-02-06 18:52:54 +0530

    update

---
+++ DifferenceofSquares.py
@@ -0,0 +1,49 @@
+import unittest
+
+
+def square_of_sum(number):
+    return sum(range(1, number + 1)) ** 2
+
+
+def sum_of_squares(number):
+    sum = 0
+    for i in range(1, number + 1):
+        sum += i * i
+    return sum
+
+
+def difference_of_squares(number):
+    return square_of_sum(number) - sum_of_squares(number)
+
+
+class DifferenceOfSquaresTest(unittest.TestCase):
+    def test_square_of_sum_1(self):
+        self.assertEqual(square_of_sum(1), 1)
+
+    def test_square_of_sum_5(self):
+        self.assertEqual(square_of_sum(5), 225)
+
+    def test_square_of_sum_100(self):
+        self.assertEqual(square_of_sum(100), 25502500)
+
+    def test_sum_of_squares_1(self):
+        self.assertEqual(sum_of_squares(1), 1)
+
+    def test_sum_of_squares_5(self):
+        self.assertEqual(sum_of_squares(5), 55)
+
+    def test_sum_of_squares_100(self):
+        self.assertEqual(sum_of_squares(100), 338350)
+
+    def test_difference_of_squares_1(self):
+        self.assertEqual(difference_of_squares(1), 0)
+
+    def test_difference_of_squares_5(self):
+        self.assertEqual(difference_of_squares(5), 170)
+
+    def test_difference_of_squares_100(self):
+        self.assertEqual(difference_of_squares(100), 25164150)
+
+
+if __name__ == "__main__":
+    unittest.main()
