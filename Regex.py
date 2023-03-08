dbc48c5eafef21f53c1700b6861f0cadeed8a5f0
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-03-08 23:12:26 +0530

    update

--- Regex.py
+++ Regex.py
@@ -1,8 +1,18 @@
 import re
 
-sentence = "From Am to Pm"
-check = re.search('^From', sentence)
-if check:
-    print('yes!')
-else:
-    print('No')
+sentence = "Some API status codes - 200, 404, 500"
+y = re.findall('[0-9]+', sentence)
+print(y)
+y = re.findall('[AEIOU]', sentence)
+print(y)
+
+
+x = 'From: Using the : character'
+y = re.findall('^F.+?:', x)
+z = re.findall('^F.+:', x)
+print(y)
+print(z)
+
+s = 'A message from csev@umich.edu to cwen@iupui.edu about meeting @2PM'
+lst = re.findall('\\S+@\\S+', s)
+print(lst)
